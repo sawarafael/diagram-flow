e667fc2f1a5f13f4dbcd56f14e14d41673c9967d
Rafael Soares <sawarafael@gmail.com>
2023-08-01 15:57:11 -0300

    feat: poc

---
+++ src/components/Diagram/index.jsx
@@ -0,0 +1,61 @@
+import React, { useCallback } from 'react';
+import ReactFlow, {
+  addEdge,
+  MiniMap,
+  Controls,
+  Background,
+  useNodesState,
+  useEdgesState,
+} from 'reactflow';
+
+import { nodes as initialNodes, edges as initialEdges } from './../../utils/data/NodesAndEdges';
+import CustomNode from './CustomNode';
+
+import 'reactflow/dist/style.css';
+import './styles.css';
+
+const DiagramProps = {
+    nodeTypes: { custom:  CustomNode},
+}
+
+const onInit = (reactFlowInstance) => console.log('flow loaded:', reactFlowInstance);
+
+const Diagram = () => {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), []);
+
+  const edgesWithUpdatedTypes = edges.map((edge) => {
+    if (edge.sourceHandle) {
+      const edgeType = nodes.find((node) => node.type === 'custom').data.selects[edge.sourceHandle];
+      edge.type = edgeType;
+    }
+
+    return edge;
+  });
+
+  return (
+        <ReactFlow
+          nodes={nodes}
+          edges={edgesWithUpdatedTypes}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
+          onConnect={onConnect}
+          onInit={onInit}
+          fitView
+          attributionPosition="top-right"
+          nodeTypes={DiagramProps.nodeTypes.custom}
+          style={{
+            height: 850,
+            width: 850
+          }}
+        >
+          <MiniMap zoomable pannable />
+          <Controls />
+          <Background color="#aaa" gap={16} />
+        </ReactFlow>    
+  );
+};
+
+export default Diagram;
---
+++ src/components/index.ts
@@ -0,0 +1,1 @@
+export { default as DiagramFlow } from './Diagram'
